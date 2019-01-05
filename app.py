ed28da707e28be4edadb25096ad801d88602fbd0
cjm322 <conor.murphy@gmail.com>
2019-01-05 18:25:15 -0500

    Added command line arguments and changed camel case to snake case

--- app.py
+++ app.py
@@ -3,24 +3,53 @@
 # any element of the set 's' will be added to a subset '(r)eservoir'
 
 import random
-import sys
+from argparse import ArgumentParser
 
-def reservoir_sample(constraint=random.randint(1, 10) , s=[i for i in range(random.randint(10,20))]):
+def reservoir_sample(constraint=random.randint(1, 9) , s=[i for i in range(random.randint(10,20))]):
   if (len(s) > constraint):
     reservoir = s[:constraint]
-    setTail = s[len(reservoir):]
-    for i in range(len(setTail)):
-      fairChoice = random.randint(0, len(reservoir) + i)
-      if fairChoice < len(reservoir):
-        reservoir[fairChoice] = setTail[i]
+    reservoir_length = len(reservoir)
+    set_tail = s[reservoir_length:]
+    set_tail_length = len(set_tail)
+    
+    for i in range(set_tail_length):
+      fair_choice = random.randint(0, reservoir_length + i)
+      if fair_choice < reservoir_length:
+        reservoir[fair_choice] = set_tail[i]
+    
+    print("Set: {0}".format(s))
     return reservoir
   else:
     print("The length of the provided set must be greater than the contraint")
     print("Constraint: %s" % constraint)
     print("Set: %s" % s)
 
-s = [i for i in range(0, 10)]
-c = 4
-print("Set: {0}".format(s))
-rs = reservoir_sample(c, s)
-print(rs)
+def parse_args():
+  parser = ArgumentParser()
+  parser.add_argument("-c", "--constraint", dest="constraint")
+  parser.add_argument("-s", "--set", dest="sample_set")
+  args = parser.parse_args()
+
+  if args.constraint:
+    try:
+      args.constraint = int(args.constraint)
+    except:
+      print("Constraint must be an integer")
+  if args.sample_set:
+    args.sample_set = [i for i in set(args.sample_set.split(","))]
+  
+  return args
+
+args = parse_args()
+
+if args.constraint and args.sample_set:
+  print(reservoir_sample(args.constraint, args.sample_set))
+
+elif args.constraint and not args.sample_set:
+  print(reservoir_sample(constraint=args.constraint))
+
+elif not args.constraint and args.sample_set:
+  print(reservoir_sample(s=args.sample_set))
+
+else:
+  print(reservoir_sample())
